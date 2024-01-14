a7bd57bf0be41a78a723bb09b1581aeb5cbb5b70
Erickmarlon006 <erickmarlonborbadesenna@gmail.com>
2024-01-14 13:18:22 -0300

    Últimos ajustes

--- scripts/script.js
+++ scripts/script.js
@@ -5,16 +5,17 @@ let nameTextField = document.getElementById("name");
 
 formulario.onsubmit = function (e) {
   e.preventDefault();
-  let resposta = confirm("Todas as informações estão corretas?");
-  if (resposta) {
-    console.log("bora");
-  } else {
-    console.log("no");
-    document.getElementById("message").textContent = "Revisa aí";
-  }
-  return resposta;
-};
-nameTextField.onkeydown = function () {
-  document.getElementById("message").textContent = "Olá " + nameTextField.value;
-  console.log("você está no campo do nome completo");
+  nameTextField.onkeydown = function () {
+    document.getElementById("message").textContent =
+      "Olá " + nameTextField.value;
+    console.log("você está no campo do nome completo");
+    let resposta = confirm("Todas as informações estão corretas?");
+    if (resposta) {
+      console.log("bora");
+      window.location.reload();
+    } else {
+      console.log("no");
+      document.getElementById("message").textContent = "Revisa aí";
+    }
+  };
 };
